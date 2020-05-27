d09bae97e406673f3adeb1820490832731d66bd6
Subham das <subhamdas226@gmail.com>
2020-05-27 21:08:41 +0530

    FSJ0320B002

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,24 @@
 package controller;
 
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClassNotFoundException, IOException, SQLException {
 		
 	// Fill your code
+		Connection con = null;
+		ConnectionManager cm = new ConnectionManager();
+		con = cm.getConnection();
 		
+		if(con != null) {
+			System.out.println("Connection is established");
+		}
+		else {
+			System.out.println("Check your connection");
+		}
 	}
 }
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,9 +1,39 @@
 package utility;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
-
+import oracle.jdbc.OracleConnection.CommitOption;
 
 public class ConnectionManager {
-	
+	public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
+		
+		Connection con= null;
+		Properties p = loadPropertiesFile();
+		String driver = p.getProperty("driver");
+		String url = p.getProperty("url");
+		String username = p.getProperty("username");
+		String password = p.getProperty("password");
+		
+		Class.forName(driver);
+		
+		con = DriverManager.getConnection(url,username,password);
+		return con;
+		
+	}
+	public static Properties loadPropertiesFile() throws IOException {
+		Properties p =new Properties();
+		FileInputStream fs = new FileInputStream("D:\\jdbc projects\\lab-jdbc-connection\\src\\jdbc.properties");
+		p.load(fs);
+		fs.close();
+		return p;
+		
+	}
 	
 }
